55b178ed43f38265dc139994faea4a5f6fc8bed1
Jeff Han <jeffninghan@gmail.com>
2014-11-08 10:51:44 -0800

    herestory

---
+++ connect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+## Connect to our EC2 instance using SSH. To do this, we have to
+## supply the PEM file using -i (Identity File) during connection.
+## For the user, we can use "ec2-user" and the public DNS value.
+ssh \
+-i ./herestory.pem \
+ec2-user@ec2-54-68-152-237.us-west-2.compute.amazonaws.com
